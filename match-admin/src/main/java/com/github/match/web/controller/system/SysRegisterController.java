e86737759c59a212e5262f42b795e7f0eecef785
haipeng-lin <haipeng_lin@163.com>
2025-07-05 22:07:15 +0800

    feat：初始化项目

---
+++ match-admin/src/main/java/com/github/match/web/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.github.match.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.github.match.common.core.controller.BaseController;
+import com.github.match.common.core.domain.AjaxResult;
+import com.github.match.common.core.domain.model.RegisterBody;
+import com.github.match.common.utils.StringUtils;
+import com.github.match.framework.web.service.SysRegisterService;
+import com.github.match.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author github.match
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
---
+++ match-common/src/main/java/com/github/match/common/exception/file/FileException.java
@@ -0,0 +1,19 @@
+package com.github.match.common.exception.file;
+
+import com.github.match.common.exception.base.BaseException;
+
+/**
+ * 文件信息异常类
+ * 
+ * @author ruoyi
+ */
+public class FileException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public FileException(String code, Object[] args)
+    {
+        super("file", code, args, null);
+    }
+
+}
